21ffafae93b09f9dd989e4b21b760300d51f5f12
mostafateto <psmostafa@gmail.com>
2021-10-07 19:27:42 +0200

    for netlify

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
   mode: mode,
   cache: { type: "filesystem" },
